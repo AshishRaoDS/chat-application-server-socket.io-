7e238353a46657aa7201adf5fe123bce96ac8427
AshishRaoDS <ashraods@gmail.com>
2023-06-01 22:33:46 +0530

    setting up file structure for auth

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const { createServer } = require("http");
 const createSocketConnection = require('./socket');
 
-const app = express();
-
 const PORT = 3000 || process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.use('/v1', api);
+
 
 const httpServer = createServer(app);
 
